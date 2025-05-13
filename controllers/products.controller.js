cc6d44a5ec4289e13146f748b1a7d6febe15ffc7
Cristian Druetta <cristian.e.druetta@gmail.com>
2025-05-13 00:25:37 -0300

    crud completo

---
+++ controllers/products.controller.js
@@ -0,0 +1,55 @@
+let products = [
+    { id: 1, nombre: 'monitor', precio: 40000 },
+    { id: 2, nombre: 'CPU Ryzen', precio: 15000 }
+]
+
+//GET obtener todos los productos
+const getProducts = (req, res) => {
+    res.json({ data: products, status: 200, mensaje: 'Lista de productos encontrada' });
+}
+
+// GET btener un producto por id
+const getProductById = (req, res) => {
+    const product = products.find(item => item.id === parseInt(req.params.id))
+    if (!product) return res.json({ status: 404, mensaje: 'Producto no encontrado' })
+
+    res.json({ data: product, status: 200, mensaje: 'Producto encontrado' })
+}
+
+//POST crear un nuevo producto
+const createProduct = (req, res) => {
+    const { nombre, precio } = req.body;
+    const newProduct = { id: products.length + 1, nombre, precio };
+    products.push(newProduct);
+
+    res.json({ data: newProduct, status: 201, mensaje: 'Producto creado' });
+}
+
+//PUT actualizar un producto
+const updateProduct = (req, res) => {
+    const product = products.find(item => item.id === parseInt(req.params.id))
+    if (!product) return res.json({ status: 404, mensaje: 'Producto no encontrado' })
+    const { nombre, precio } = req.body;
+    product.nombre = nombre || product.nombre;
+    product.precio = precio || product.precio;
+
+    res.json({ data: product, status: 200, mensaje: 'Producto actualizado' });
+}
+
+//DELETE eliminar un producto
+const deleteProduct = (req, res) => {
+    const product = products.find(item => item.id === parseInt(req.params.id))
+    if (!product) return res.json({ status: 404, mensaje: 'Producto no encontrado' })
+    products = products.filter(item => item.id !== product.id)
+
+    res.json({ status: 201, mensaje: 'Producto eliminado' });
+}
+
+// Exportar los controladores
+module.exports = {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
--- index.js
+++ index.js
@@ -6,27 +6,8 @@ const port = 3000
 
 app.use(express.json());
 
-
-const productos = [
-    { id: 1, nombre: 'monitor', precio: 40000 },
-    { id: 2, nombre: 'CPU Ryzen', precio: 15000 }
-]
-
-app.get('/productos', (req, res) => {
-    res.json(productos);
-});
-
-
-app.get('/bienvenida', (req, res) => {
-    res.send('Bienvenido al sistema');
-})
-
-app.post('/saludo', (req, res) => {
-    const { nombre } = req.body
-
-    res.json({
-        mensaje: `Hola ${nombre}, bienvenido a la api`, status: 200})
-})
+const productosRoutes = require('./routes/products.routes')
+app.use('/productos', productosRoutes);
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
